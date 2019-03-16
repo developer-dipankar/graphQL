a376c357bf3d393eae5c6f173e949dd4dcbee864
Dipankar <dnaskar@dccservices.net>
2019-03-16 22:19:39 +0530

    GraphQL Subscription Basics

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-import { GraphQLServer } from 'graphql-yoga';
-import uuidv4 from 'uuid/v4';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import db from './db';
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import User from './resolvers/User'
 
+const pubsub = new PubSub();
 const server = new GraphQLServer({
     'typeDefs': './schema.graphql',
     'resolvers': {
         Query,
         Mutation,
+        Subscription,
         User
     },
     context: {
-        db
+        db,
+        pubsub
     }
 });
 server.start(() => console.log('GraphQL Server started'))

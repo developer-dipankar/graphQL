2d179e940a016464f2783363e9ff74bb51943976
Dipankar <dnaskar@dccservices.net>
2019-03-16 21:21:35 +0530

    Updating Data with Mutations: Part II

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const users = [
     {id: '1', fullname: 'Andrew Parker', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,2]},
-    {id: '2', fullname: 'Molly Duncan', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,3]},
-    {id: '3', fullname: 'Steve Smith', email: 'abc@domain.com', age: 29, isActive: true, posts: [3,2]},
-    {id: '4', fullname: 'Prithi Potolkumari', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,4]},
-    {id: '5', fullname: 'Ken Takla', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,2,5]},
-    {id: '6', fullname: 'Leo Kabla', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,5]},
-    {id: '7', fullname: 'Kaan dhore mule debo', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,3]},
-    {id: '8', fullname: 'Keliye laat kore debo', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,4]},
+    {id: '2', fullname: 'Molly Duncan', email: 'abcd@domain.com', age: 29, isActive: true, posts: [1,3]},
+    {id: '3', fullname: 'Steve Smith', email: 'abce@domain.com', age: 29, isActive: true, posts: [3,2]},
+    {id: '4', fullname: 'Prithi Potolkumari', email: 'abcf@domain.com', age: 29, isActive: true, posts: [1,4]},
+    {id: '5', fullname: 'Ken Takla', email: 'abcg@domain.com', age: 29, isActive: true, posts: [1,2,5]},
+    {id: '6', fullname: 'Leo Kabla', email: 'abch@domain.com', age: 29, isActive: true, posts: [1,5]},
+    {id: '7', fullname: 'Kaan dhore mule debo', email: 'abci@domain.com', age: 29, isActive: true, posts: [1,3]},
+    {id: '8', fullname: 'Keliye laat kore debo', email: 'abcj@domain.com', age: 29, isActive: true, posts: [1,4]},
 ]
 
 const posts = [
--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -16,6 +16,36 @@ const Mutation = {
         return user;
     },
 
+    updateUser(parent, {id, data}, {db}, info) {
+        const checkUser = db.users.findIndex((item) => item.id === id);
+        if(checkUser === -1) {
+            throw new Error('User not found');
+        }
+        
+        const checkEmail = db.users.findIndex((item) => item.email === data.email && item.id !== id)
+        if(checkEmail !== -1) {
+            throw new Error('Email is already existed');
+        }
+
+        if(typeof data.email === 'string') {
+            db.users[checkUser].email = data.email;
+        }
+
+        if(typeof data.fullname === 'string') {
+            db.users[checkUser].fullname = data.fullname;
+        }
+
+        if(typeof data.age === 'number') {
+            db.users[checkUser].age = data.age;
+        }
+
+        if(typeof data.isActive === 'boolean') {
+            db.users[checkUser].isActive = data.isActive;
+        }
+
+        return db.users[checkUser];
+    },
+
     createPost(parent, args, {db}, info) {
         const post = {
             id: uuidv4(),

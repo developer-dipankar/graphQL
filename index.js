50b26167a7965a124cded0ae8e5c0f326d3591a5
Dipankar <dnaskar@dccservices.net>
2019-03-16 11:43:48 +0530

    User data changed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { GraphQLServer } from 'graphql-yoga';
 
 const users = [
-    {id: '1', fullname: 'Andrew', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,2]},
-    {id: '2', fullname: 'Molly', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,3]},
-    {id: '3', fullname: 'Hayden', email: 'abc@domain.com', age: 29, isActive: true, posts: [3,2]},
-    {id: '4', fullname: 'Jitendra', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,4]},
-    {id: '5', fullname: 'Somen Gadha', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,2,5]},
-    {id: '6', fullname: 'Abhijit Chagol', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,5]},
-    {id: '7', fullname: 'Gandu', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,3]},
-    {id: '8', fullname: 'Pagla C***a', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,4]},
+    {id: '1', fullname: 'Andrew Parker', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,2]},
+    {id: '2', fullname: 'Molly Duncan', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,3]},
+    {id: '3', fullname: 'Steve Smith', email: 'abc@domain.com', age: 29, isActive: true, posts: [3,2]},
+    {id: '4', fullname: 'Prithi Potolkumari', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,4]},
+    {id: '5', fullname: 'Ken Takla', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,2,5]},
+    {id: '6', fullname: 'Leo Kabla', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,5]},
+    {id: '7', fullname: 'Kaan dhore mule debo', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,3]},
+    {id: '8', fullname: 'Keliye laat kore debo', email: 'abc@domain.com', age: 29, isActive: true, posts: [1,4]},
 ]
 
 const posts = [
